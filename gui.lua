941afc0822f0010120342d678c6987f9ee5f69e7
Matt McGinty <matt.mcginty89@gmail.com>
2019-04-08 19:11:31 +0100

    Improved UI appearance

--- gui.lua
+++ gui.lua
@@ -3,6 +3,21 @@ require "const"
 config = require('config')
 texts = require('texts')
 
+defaults = {}
+defaults.pos = {}
+defaults.pos.x = 300
+defaults.pos.y = 475
+defaults.text = {}
+defaults.text.font = 'Arial'
+defaults.text.size = 8
+defaults.flags = {}
+defaults.flags.bold = true
+defaults.flags.draggable = true
+defaults.bg = {}
+defaults.bg.alpha = 128
+
+settings = config.load(defaults)
+
 listGUI = texts.new(settings)
 settings = config.load()
 
